ca40d07d33fb7f341d01312102d992f86afdb1b1
abdur-rehman-ali <alijoyia911@gmail.com>
2022-03-08 09:19:15 +0500

    login and register views updated

--- main/views.py
+++ main/views.py
@@ -9,44 +9,45 @@ def home(request):
 
 
 def register(request):
-    if request.method == "POST":
-        form = registrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            form = registrationForm()
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/')
     else:
-        form = registrationForm()
-    template_name = 'main/register.html'
-    context={
-        'form':form
-    }
-    return render(request,template_name,context)
+        if request.method == "POST":
+            form = registrationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                form = registrationForm()
+        else:
+            form = registrationForm()
+        template_name = 'main/register.html'
+        context={
+            'form':form
+        }
+        return render(request,template_name,context)
+
 
 def logIn(request):
-    template_name = 'main/logIn.html'
-    context = {
-
-    }
-    return render(request,template_name,context)
-
-def logIn(request):
-    if request.method=="POST":
-        fm=logInForm(request=request,data=request.POST)
-        if fm.is_valid():
-            uname=fm.cleaned_data['username']
-            upass=fm.cleaned_data['password']
-            user=authenticate(username=uname,password=upass)
-            if user is not None:
-                login(request,user)
-                return HttpResponseRedirect('/')
-    else:        
-        fm=logInForm()
-    template_name = 'main/logIn.html'
-    context = {
-        'form':fm
-    }
-    return render(request,template_name,context)
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/')
+    else:
+        if request.method=="POST":
+            fm=logInForm(request=request,data=request.POST)
+            if fm.is_valid():
+                uname=fm.cleaned_data['username']
+                upass=fm.cleaned_data['password']
+                user=authenticate(username=uname,password=upass)
+                if user is not None:
+                    login(request,user)
+                    return HttpResponseRedirect('/')
+        else:        
+            fm=logInForm()
+        template_name = 'main/logIn.html'
+        context = {
+            'form':fm
+        }
+        return render(request,template_name,context)
 
 def logOut(request):
     logout(request)
     return HttpResponseRedirect('logIn')
+    
