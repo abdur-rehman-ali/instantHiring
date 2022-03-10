eb503a543dbbe19a008fbe0e8254fd0020efe8a3
abdur-rehman-ali <alijoyia911@gmail.com>
2022-03-10 05:24:42 +0500

    registered model in django admin application

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import jobPostData
 
 # Register your models here.
+
+@admin.register(jobPostData)
+class jobPostDataAdmin(admin.ModelAdmin):
+    list_display = ['title','location','job_nature','category']
