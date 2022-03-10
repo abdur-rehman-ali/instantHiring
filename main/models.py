04173114c92df321ea80a5b64f0db70528a233a7
abdur-rehman-ali <alijoyia911@gmail.com>
2022-03-10 05:34:13 +0500

    choices added to models for category

--- main/models.py
+++ main/models.py
@@ -1,15 +1,27 @@
+from sre_constants import CATEGORY
 from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 class jobPostData(models.Model):
+    CATEGORY_CHOICES = (
+    ("Design & Creative", "Design & Creative"),
+    ("Design & Development", "Design & Development"),
+    ("Sales & Marketing", "Sales & Marketing"),
+    ("Mobile Application", "Mobile Application"),
+    ("Construction", "Construction"),
+    ("Information Technology", "Information Technology"),
+    ("Real Estate", "Real Estate"),
+    ("Content Writer", "Content Writer"),
+    )
+
     title = models.CharField(max_length=70)
     desription = models.TextField()
     author = models.CharField(max_length=50)
     location = models.CharField(max_length=100)
     job_nature = models.CharField(max_length=30)
-    category = models.CharField(max_length=50)
+    category = models.CharField(max_length=50,choices=CATEGORY_CHOICES,default='No category')
     date = models.DateTimeField(auto_now_add=True)
     user_job_post = models.ForeignKey(User,on_delete=models.CASCADE)
 
